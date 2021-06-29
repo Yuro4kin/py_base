1a78a5fba4dfa3d6036b71c4e76057d916211349
Yuro4kin <65240198+Yuro4kin@users.noreply.github.com>
2021-06-29 11:43:47 +0300

    Add files via upload

---
+++ programs/test_7.2.py
@@ -0,0 +1,12 @@
+
+
+
+
+# 2. Из строки «abrakadabra» удалите все сочетания «ab»
+word = input("Введите слово: ")
+#msg = "abracadabra"
+wordDel = word.replace("ab", "")
+print("Из строки «______» удалены все сочетания «ab»: ", wordDel)
+
+
+
