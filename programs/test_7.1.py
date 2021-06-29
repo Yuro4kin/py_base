049a9624b8e8b2e154dc0944c245a9b120dec6c4
Yuro4kin <65240198+Yuro4kin@users.noreply.github.com>
2021-06-29 11:37:43 +0300

    Add files via upload

---
+++ programs/test_7.1.py
@@ -0,0 +1,10 @@
+
+
+# 1. Подсчет букв "a" в строке "abrakadabra"
+word = input("Введите слово: ")
+# msg = "abracadabra"
+msgA = word.count("a")
+print("Выполнен подсчет букв (a) в строке: ", msgA)
+
+
+
